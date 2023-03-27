853c0b267241c67bf7ae4f94bdbf9eddf2007c73
Alexander Parco <alexparco16@gmail.com>
2023-03-27 10:45:50 -0500

    servidor estatico de express

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-const http = require('http')
-fs = require('fs')
+const express = require('express')
 
+const app = express()
 
-const server = http.createServer((solicitud, respuesta) => {
-  fs.readFile('view/main.html', (error, html) => {
-    respuesta.write(html)
-    respuesta.end()
-  })
+const PORT = 3030
+
+// static files
+app.use(express.static('src'));
+
+app.get('/', (req, res) => { })
+
+app.use((req, res) => {
+  res.status(404).send('No se encontro tu pagina......!!')
 })
 
-server.listen(8080)
-
-
+app.listen(PORT, () => {
+  console.log(`Server on port ${PORT}`)
+})
